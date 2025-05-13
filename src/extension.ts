02c7885eeb92d361c9cf7ceca2ccbc53ca76436f
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-05-13 23:23:24 +0530

    minor testing with vscode extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { printHello } from './lib/test';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -16,10 +17,15 @@ export function activate(context: vscode.ExtensionContext) {
 	const disposable = vscode.commands.registerCommand('looper.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from looper!');
+		// vscode.window.showInformationMessage('Hello World from looper! we are live');
+    printHello();
 	});
+  const check1 = vscode.commands.registerCommand('looper.monitor', () => {
+    vscode.window.showInformationMessage("monitoring event loop");
+  });
 
 	context.subscriptions.push(disposable);
+  context.subscriptions.push(check1);
 }
 
 // This method is called when your extension is deactivated
---
+++ src/lib/test.ts
@@ -0,0 +1,4 @@
+import * as vscode from 'vscode';
+export function printHello(){
+  vscode.window.showInformationMessage(" hello we are live from looper, how are you ?");
+}
